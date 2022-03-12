053f96a0e30df9eb87ae863a5b8a8b5281353358
THINH DOAN VU <thinhdv@ntu.edu.vn>
2022-03-12 11:59:28 +0700

    Update Youtubeview.bat

--- Youtubeview.bat
+++ Youtubeview.bat
@@ -1,5 +1,5 @@
 @echo OFF
-MicrosoftEdge https://www.youtube.com/watch?v=PJcyj6SL6tM
+MicrosoftEdge https://www.youtube.com/watch?v=5YoQ5EjwYVo&list=PL_Ks8fYUz-ApWY-_klKTrI0xQn8SVXQpH
 for /L %%i IN (1,1,300) DO (
 	echo %%i
 	xdotool.exe key "Control_L+R"
